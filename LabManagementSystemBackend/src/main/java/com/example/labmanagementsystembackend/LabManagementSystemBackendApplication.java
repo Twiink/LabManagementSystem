36c5cbdd5379c11e50f91f12f011a08bc1d9984e
Twiink <zy13990529155@163.com>
2025-12-26 12:59:04 +0800

    项目初始化

---
+++ LabManagementSystemBackend/src/main/java/com/example/labmanagementsystembackend/LabManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.labmanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabManagementSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabManagementSystemBackendApplication.class, args);
+    }
+
+}
---
+++ LabManagementSystemBackend/src/test/java/com/example/labmanagementsystembackend/LabManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labmanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabManagementSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
